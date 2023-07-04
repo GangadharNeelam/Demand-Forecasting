713a2e1af20534bb4f85e21a9bdfc0089df6beba
GANGADHARA GANNIBABU NEELAM <20p35a03a6@acet.ac.in>
2023-07-05 03:04:40 +0530

    First commit

--- app5.py
+++ app5.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import load_model
 import plotly.express as px
 import plotly.graph_objects as go
+from PIL import Image
 
 # Load the model
 model = load_model('lstm_model_final.h5')
@@ -65,55 +66,60 @@ def main():
     st.markdown("Forecast future order demand using LSTM model")
 
     # Sidebar
-    st.sidebar.subheader("Settings")
-    future_months = st.sidebar.number_input('Enter the number of months to forecast', min_value=1, value=6, step=1)
+    st.sidebar.subheader("Navigation")
+    section = st.sidebar.radio("", ["Forecast", "About"])
 
-    if st.sidebar.button('Predict'):
-        predictions = []
-        current_data = scaled_data
+    if section == "Forecast":
+        st.subheader("Forecast")
+        st.markdown("---")
+        st.sidebar.write("Click the button below to predict the order demand for the future months.")
+        future_months = st.sidebar.number_input('Enter the number of months to forecast', min_value=1, value=6, step=1)
+        
+        if st.sidebar.button('Predict'):
+            predictions = []
+            current_data = scaled_data
 
-        for _ in range(future_months):
-            prediction = predict_next_month(current_data, model, scaler, seq_length)
-            predictions.append(prediction)
-            current_data = np.concatenate((current_data[1:], prediction.reshape(-1, 1)))
+            for _ in range(future_months):
+                prediction = predict_next_month(current_data, model, scaler, seq_length)
+                predictions.append(prediction)
+                current_data = np.concatenate((current_data[1:], prediction.reshape(-1, 1)))
 
-        # Print the predicted order demand for the future months
-        next_months_dates = pd.date_range(start=data.index[-1], periods=future_months + 1, freq='M')[1:]
-        predicted_demand = pd.DataFrame(np.round(predictions), index=next_months_dates, columns=['Order Demand'])
+            # Print the predicted order demand for the future months
+            next_months_dates = pd.date_range(start=data.index[-1], periods=future_months + 1, freq='M')[1:]
+            predicted_demand = pd.DataFrame(np.round(predictions), index=next_months_dates, columns=['Order Demand'])
 
-        predicted_demand.reset_index(inplace=True)
-        predicted_demand.rename(columns={'index': 'Date'}, inplace=True)
-        predicted_demand["Date"] = pd.to_datetime(predicted_demand["Date"]).dt.strftime('%b %Y')
+            predicted_demand.reset_index(inplace=True)
+            predicted_demand.rename(columns={'index': 'Date'}, inplace=True)
+            predicted_demand["Date"] = pd.to_datetime(predicted_demand["Date"]).dt.strftime('%b %Y')
 
-        # Display forecasted values
-        st.subheader('Forecasted values:')
-        st.dataframe(predicted_demand.style.set_properties(**{'text-align': 'center'}))
+            # Display forecasted values
+            st.subheader('Forecasted values:')
+            st.dataframe(predicted_demand.style.set_properties(**{'text-align': 'center'}))
 
-        # Plot forecasted order demand over time
-        fig = go.Figure(data=go.Scatter(x=predicted_demand['Date'], y=predicted_demand['Order Demand'], mode='lines',
-                                        name='Forecasted Demand', line=dict(color='#F63366', width=2)))
-        fig.update_layout(
-            title='Forecasted Order Demand Over Time',
-            xaxis_title='Date',
-            yaxis_title='Order Demand',
-            plot_bgcolor='white',
-            paper_bgcolor='rgba(0,0,0,0)',
-            font_color='black'
-        )
-        st.plotly_chart(fig)
+            # Plot forecasted order demand over time
+            fig = go.Figure(data=go.Scatter(x=predicted_demand['Date'], y=predicted_demand['Order Demand'], mode='lines',
+                                            name='Forecasted Demand', line=dict(color='#F63366', width=2)))
+            fig.update_layout(
+                title='Forecasted Order Demand Over Time',
+                xaxis_title='Date',
+                yaxis_title='Order Demand',
+                plot_bgcolor='white',
+                paper_bgcolor='rgba(0,0,0,0)',
+                font_color='black'
+            )
+            st.plotly_chart(fig)
 
-    # About section
-    st.markdown("---")
-    st.subheader("About")
-    st.markdown("The purpose of this app is to forecast the monthly order demand using an LSTM model.")
-    st.markdown("It addresses order shortage issues related to ocean shipping, where orders can take months or weeks to arrive.")
-    st.markdown("To use the app, enter the number of months to forecast in the sidebar and click the 'Predict' button.")
-    st.markdown("The app will generate forecasted values and display graphs for visualization.")
+    elif section == "About":
+        st.subheader("About")
+        st.markdown("---")
+        st.markdown("The purpose of this app is to forecast the monthly order demand using an LSTM model.")
+        st.markdown("It addresses order shortage issues related to ocean shipping, where orders can take months or weeks to arrive.")
+        st.markdown("To use the app, select the 'Forecast' section and enter the number of months to forecast in the sidebar. Click the 'Predict' button to generate forecasted values and visualize the results.")
 
     # Footer
     st.markdown("---")
-    st.markdown("Developed by Gangadhar")
-    st.markdown("Connect with me on [LinkedIn](https://www.linkedin.com/in/gangadhar-neelam/) | [GitHub](https://github.com/GangadharNeelam)")
+    st.markdown("Connect with me:")
+    st.markdown("[![LinkedIn](https://raw.githubusercontent.com/GangadharNeelam/Streamlit-Demand-Forecasting-App/main/assets/linkedin_logo.png)](https://www.linkedin.com/in/gangadhar-neelam/) [![GitHub](https://raw.githubusercontent.com/GangadharNeelam/Streamlit-Demand-Forecasting-App/main/assets/github_logo.png)](https://github.com/GangadharNeelam)")
 
 if __name__ == '__main__':
     main()
